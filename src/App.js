a8cf0f42a96d9f066018da65e3fd1aedd08be697
Italoe <2d.italo@gmail.com>
2022-12-14 10:18:26 -0300

    component Timer criado

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Timer from './components/Timer/index.jsx'
 
 function App() {
   return (
     <div className="App">
-      Countdown Timer
+      <Timer />
     </div>
   );
 }
---
+++ src/components/Timer/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Timer = () => {
+  return (
+    <div>Timer created!</div>
+  )
+}
+
+export default Timer
