a0cde358c6ea3395ac047a09a8741266a97163b9
Italoe <2d.italo@gmail.com>
2022-12-15 08:15:34 -0300

    countdown implementado utilizando a biblioteca react-countdown

--- src/components/Timer/index.jsx
+++ src/components/Timer/index.jsx
@@ -1,15 +1,31 @@
 import { Box } from '@mui/material'
 import React from 'react'
 import './style.css'
+import Countdown from 'react-countdown';
 
+const Timer = () => {  
+  const Completionist = () => <span>You are good to go!</span>;
+  const renderer = ({ days, hours, minutes, seconds, completed }) => {
+    console.log(days + " " + hours);
+    if (completed) {
+      // Render a completed state
+      return <Completionist />;
+    } else {
+      // Render a countdown
+      return <span>{days}:{hours}:{minutes}:{seconds}</span>;
+    }
+  };
 
-
-
-const Timer = () => {
   return (
     <Box className='box'>
       <div className="container">
-    
+        <div className='content'>we're lauching soon</div>
+        <Countdown
+          className='content'
+          date={Date.parse("2025-01-01")} //Date must be modified here using ISO 8601 calendar date extended format.
+          renderer={renderer}
+        />
+        <div className='content'>end</div>
       </div>
     </Box>
   )
