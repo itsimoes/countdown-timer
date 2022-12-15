1de2e8b61ab67d1081676e6234a7f9d1eeb092f6
Italoe <2d.italo@gmail.com>
2022-12-15 14:42:02 -0300

    alinhamento de blocos feito e contador estilizado (ainda sem animacao)

--- src/components/Timer/index.jsx
+++ src/components/Timer/index.jsx
@@ -3,8 +3,10 @@ import React from 'react'
 import './style.css'
 import Countdown from 'react-countdown';
 
-const Timer = () => {  
+const Timer = () => {
+
   const Completionist = () => <span>You are good to go!</span>;
+
   const renderer = ({ days, hours, minutes, seconds, completed }) => {
     console.log(days + " " + hours);
     if (completed) {
@@ -12,25 +14,28 @@ const Timer = () => {
       return <Completionist />;
     } else {
       // Render a countdown
-      return <span>{days}:{hours}:{minutes}:{seconds}</span>;
+      return <div className='central-box'>
+        <div><span className='count-box'>{days}</span><div className='f-text'>days</div></div>
+        <div><span className='count-box'>{hours}</span><div className='f-text'>hours</div></div>
+        <div><span className='count-box'>{minutes}</span><div className='f-text'>minutes</div></div>
+        <div><span className='count-box'>{seconds}</span><div className='f-text'>seconds</div></div>
+        </div>;
     }
   };
 
   return (
     <Box className='box'>
       <div className="container">
-        <div className='content'>we're lauching soon</div>
+        <div className='content top-box'>we're lauching soon</div>
         <Countdown
-          className='content'
-          date={Date.parse("2025-01-01")} //Date must be modified here using ISO 8601 calendar date extended format.
+          // date={Date.parse("2025-01-01")} // Used in real situations - Date must be modified here using ISO 8601 calendar date extended format.
+          date={Date.now() + 1209600000 }    // Used for FrontEnd Mentor challenge
           renderer={renderer}
         />
-        <div className='content'>end</div>
-      </div>
+        <div className='content bottom-box'>end</div>        
+      </div>      
     </Box>
   )
-
-
 }
 
 export default Timer
